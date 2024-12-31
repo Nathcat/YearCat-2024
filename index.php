21755a60b3c3291d01a988dac82b0fa663f73348
Nathan Baines <supernath126@gmail.com>
2024-12-31 16:46:53 +0000

    Rounded percentage

--- index.php
+++ index.php
@@ -84,7 +84,7 @@
                                 };
                             }
                         
-                            $("#user-puzzles-solved").text("You solved " + r["puzzlesSolved"] + " of those puzzles! Thats " + ((r["puzzlesSolved"] / 163) * 100) + "% of all puzzles!");
+                            $("#user-puzzles-solved").text("You solved " + r["puzzlesSolved"] + " of those puzzles! Thats " + Math.round(((r["puzzlesSolved"] / 163) * 100), 1) + "% of all puzzles!");
                         });
                     </script>
                 </div>
